5fb5ccc183d1db9587a2e3c12145fcf23f7a5e36
Audrey-Costa <audreycosta@nano.ufrj.br>
2023-05-14 13:51:19 -0300

    feat(Server): Create ErrorHandler Middleware

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,13 @@
 import dotenv from "dotenv";
 import express from "express";
+import "express-async-errors";
 import cors from "cors";
+import errorHandlerMiddleware from "./middlewares/errorHandlerMiddleware";
+
 
 const app = express();
 app.use(cors({origin: '*'}), express.json());
+app.use(errorHandlerMiddleware);
 
 dotenv.config();
 
