29148a2bdc68c38d2c6d75c84e511c13326de86c
Audrey-Costa <audreycosta@nano.ufrj.br>
2023-05-14 12:33:26 -0300

    feat(App): First Commit
    Initial application settings.

---
+++ Web/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Logo from "./assets/Shopper_logo.png";
+
+function App() {
+  return (
+    <>
+      <img src={Logo} alt="Shopper" className='logo'/>
+    </>
+  )
+}
+
+export default App
